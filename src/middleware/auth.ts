f7f82e2361e60ea3bd7c1a9cb17d5631b5ff7ae5
Reketino <bjornevensk8@gmail.com>
2025-12-04 15:37:32 +0100

    connected the MongoDB wagon

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,30 +2,27 @@ import type { Request, Response, NextFunction } from "express";
 import { getEnv } from "../utils/env.js";
 import jwt, { type Secret } from "jsonwebtoken";
 
-
 export function auth(req: Request, res: Response, next: NextFunction) {
-    const header = req.headers.authorization;
+  const header = req.headers.authorization;
 
+  if (!header) {
+    return res.status(401).json({ msg: "Token er fortapt" });
+  }
 
-    if(!header) { 
-        return res.status(401).json({ msg: "Token er fortapt" });
-    }
+  const parts = header.split(" ");
 
-const parts = header.split(" ");
-
-if (parts.length < 2) {
+  if (parts.length < 2) {
     return res.status(401).json({ msg: "Token er helt fortapt" });
-}
+  }
 
+  const token = parts[1]!;
+  const secret: Secret = getEnv("JWT_SECRET") as Secret;
 
-const token = parts[1]!;
-const secret: Secret = getEnv("JWT_SECRET") as Secret;
-
-    try {
-        const decoded = jwt.verify(token, secret);
-        (req as any).user = decoded;
-        next();
-    } catch {
-        return res.status(401).json({ msg: "Token ikke gyldig"})
-    }
+  try {
+    const decoded = jwt.verify(token, secret);
+    (req as any).user = decoded;
+    next();
+  } catch {
+    return res.status(401).json({ msg: "Token ikke gyldig" });
+  }
 }
